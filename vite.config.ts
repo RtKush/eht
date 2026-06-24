9f7ce3565029f730bbeb192274b3505c679a3586
RtKush <kush22aur@gmail.com>
2026-06-24 22:07:38 +0530

    fix dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
@@ -16,8 +17,8 @@ export default defineConfig({
   base: basePath,
   plugins: [
     react(),
-   
-    runtimeErrorOverlay(),
+  tailwindcss(),
+  runtimeErrorOverlay(),
     ...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
       ? [
